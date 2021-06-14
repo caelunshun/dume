d2876f308798a8430cac8eb78da5996caf128bf6
caelunshun <caelunshun@gmail.com>
2021-06-14 14:10:55 -0600

    Add rich text to C++ bindings

--- example/sprite.cpp
+++ example/sprite.cpp
@@ -1,5 +1,13 @@
 #include <dume.h>
 #include <vector>
+#include <fstream>
+
+std::string loadFile(const std::string &path) {
+    std::ifstream t(path);
+    std::string str((std::istreambuf_iterator<char>(t)),
+                    std::istreambuf_iterator<char>());
+    return str;
+}
 
 int main() {
     glfwInit();
@@ -10,18 +18,36 @@ int main() {
     std::vector<uint8_t> rgba(128 * 128 * 4);
     for (int x = 0; x < 128; x++) {
         for (int y = 0; y < 128; y++) {
-            rgba[(x + y * 128) * 4] = x * 2;
-            rgba[(x + y * 128) * 4 + 1] = x * 2;
-            rgba[(x + y * 128) * 4 + 2] = x * 2;
+            rgba[(x + y * 128) * 4] = x;
+            rgba[(x + y * 128) * 4 + 1] = x;
+            rgba[(x + y * 128) * 4 + 2] = x;
             rgba[(x + y * 128) * 4 + 3] = 255;
         }
     }
     auto sprite = canvas.createSpriteFromRGBA("sprite", rgba.data(), rgba.size(), 128, 128);
 
+    canvas.loadFont(loadFile("/home/caelum/Downloads/Merriweather-Regular.ttf"));
+    canvas.loadFont(loadFile("/home/caelum/Downloads/Merriweather-Italic.ttf"));
+    canvas.loadFont(loadFile("/home/caelum/Downloads/Merriweather-Bold.ttf"));
+    canvas.loadFont(loadFile("/home/caelum/Downloads/Merriweather-BoldItalic.ttf"));
+
+    auto text = canvas.parseTextMarkupDefault("@size{30}{I am @bold{Dume}. @italic{I am the Bendu.}}");
+    auto paragraph = canvas.createParagraph(text, TextLayout {
+            .max_dimensions = Vec2 {
+                    .x = 1920 / 2,
+                    .y = 1080 / 2,
+            },
+            .line_breaks = true,
+            .baseline = Baseline::Top,
+         .align_h = Align::Center,
+         .align_v = Align::Center,
+    });
+
     while (!glfwWindowShouldClose(window)) {
         canvas.drawSprite(sprite, 30, 30, 600);
-        canvas.render();
+        canvas.drawParagraph(paragraph, 0, 0);
 
+        canvas.render();
         glfwPollEvents();
     }
 
--- include/dume.h
+++ include/dume.h
@@ -12,6 +12,10 @@
 #include <GLFW/glfw3native.h>
 
 namespace dume {
+    static Variable resolveDefaultVariable(void *userdata, const uint8_t *name, size_t len) {
+        return Variable{.value = nullptr, .len=0};
+    }
+
     class Canvas {
         DumeCtx *ctx;
 
@@ -38,6 +42,26 @@ namespace dume {
             return dume_create_sprite_from_rgba(ctx, reinterpret_cast<const uint8_t *>(name.data()), name.size(),
                                                    data, dataSize,
                                                    width, height);
+        }
+
+        void loadFont(std::string fontData) {
+            dume_load_font(ctx, reinterpret_cast<const uint8_t *>(fontData.data()), fontData.size());
+        }
+
+        Text *parseTextMarkup(const std::string &markup, void *userdata, Variable(*resolveVariable) (void *, const uint8_t *, size_t)) {
+            return dume_parse_markup(reinterpret_cast<const uint8_t *>(markup.data()), markup.size(), userdata, resolveVariable);
+        }
+
+        Text *parseTextMarkupDefault(const std::string &markup) {
+            return parseTextMarkup(markup, nullptr, resolveDefaultVariable);
+        }
+
+        Paragraph *createParagraph(Text *text, TextLayout layout) {
+            return dume_create_paragraph(ctx, text, layout);
+        }
+
+        void drawParagraph(const Paragraph *paragraph, float x, float y) {
+            dume_draw_paragraph(ctx, Vec2{ .x = x, .y = y }, paragraph);
         }
 
         void drawSprite(uint64_t sprite, float x, float y, float width) {
