d08e20889d7a7c1df9250fa5769885eb2f07258c
caelunshun <caelunshun@gmail.com>
2021-06-18 00:05:59 -0600

    Add Fixed widget

---
+++ ui/widget/fixed.lua
@@ -0,0 +1,14 @@
+-- Sets the size of a child.
+local Fixed = {}
+
+function Fixed:new(child, size)
+    local o = { child = child, children = {child}, params = { size = size } }
+end
+
+function Fixed:layout(maxSize, cv)
+    self.child:layout(self.params.size, cv)
+    self.size = self.params.size
+    self.child.pos = Vector(0, 0)
+end
+
+return Fixed
