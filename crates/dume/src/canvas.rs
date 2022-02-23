0e95b5f6b65c10fcf0f3f0d8c00fc038d315868d
caelunshun <caelunshun@gmail.com>
2022-02-23 00:57:05 -0700

    Reimplement embedded icons in text

--- crates/dume/src/canvas.rs
+++ crates/dume/src/canvas.rs
@@ -345,7 +345,12 @@ impl Canvas {
                     self.draw_glyph(*glyph_id, glyph.font, *size, color, pos + glyph.pos);
                 }
                 GlyphCharacter::LineBreak => {}
-                GlyphCharacter::Icon(_, _) => todo!(),
+                GlyphCharacter::Icon(texture_id, size) => {
+                    let dims = self.context.texture_dimensions(*texture_id);
+                    let aspect_ratio = dims.y as f32 / dims.x as f32;
+                    let height = aspect_ratio * *size;
+                    self.draw_sprite(*texture_id, glyph.pos - vec2(0., height), *size);
+                }
             }
         }
         self
